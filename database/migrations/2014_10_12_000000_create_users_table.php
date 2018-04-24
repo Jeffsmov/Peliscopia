29bb43cda20fa96a52dd0b96a331300d023d2364
Miguel Bonilla <32287228+Jeffsmov@users.noreply.github.com>
2018-04-23 23:58:30 -0500

    0.2 Laravel Base
    Base de Laravel.
    
    Add:
    -Laravel
    -Database Migration/Seed
    
    Lacks:
    -Blades/Views

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+			$table->date('fechaNacimiento');
+			$table->integer('genero');
+			$table->integer('tipoUsuario');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+		DB::statement("ALTER TABLE user ADD ftPerfil MEDIUMBLOB null");
+		DB::statement("ALTER TABLE user ADD bio MEDIUMTEXT null");
+		
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
